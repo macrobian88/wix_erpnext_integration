fcd9c6b9ab548191fe32ea3f12ad25a60367314b
macrobian88 <ramshankarsc9@gmail.com>
2025-09-17 12:43:07 +0530

    Fix hooks.py location - move to correct directory

---
+++ wix_integration/wix_integration/hooks.py
@@ -0,0 +1,46 @@
+app_name = "wix_integration"
+app_title = "Wix Integration"
+app_publisher = "Your Company"
+app_description = "Bidirectional sync between Wix eCommerce and ERPNext"
+app_email = "admin@yourcompany.com"
+app_license = "MIT"
+app_version = "1.0.0"
+required_apps = ["frappe", "erpnext"]
+
+# Fixtures
+fixtures = [
+    {
+        "doctype": "Custom Field",
+        "filters": {
+            "dt": ["in", ["Item", "Sales Order", "Customer"]],
+            "fieldname": ["like", "wix_%"]
+        }
+    }
+]
+
+# Document Events
+doc_events = {
+    "Item": {
+        "after_insert": "wix_integration.wix_integration.api.product_sync.sync_product_to_wix",
+        "on_update": "wix_integration.wix_integration.api.product_sync.sync_product_to_wix",
+        "on_trash": "wix_integration.wix_integration.api.product_sync.delete_product_from_wix"
+    },
+    "Sales Order": {
+        "after_insert": "wix_integration.wix_integration.api.order_sync.process_wix_order"
+    }
+}
+
+# Scheduled Tasks
+scheduler_events = {
+    "hourly": [
+        "wix_integration.wix_integration.tasks.sync_orders.sync_wix_orders_to_erpnext"
+    ],
+    "daily": [
+        "wix_integration.wix_integration.tasks.sync_inventory.sync_inventory_to_wix"
+    ]
+}
+
+# Website route rules
+website_route_rules = [
+    {"from_route": "/api/wix-webhook", "to_route": "wix_integration.wix_integration.api.webhooks.handle_wix_webhook"}
+]
